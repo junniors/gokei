9f860c64205f33d864841c8a9555fed8c7db7de0
José Agnaldo Jr. <agjunior.sd@gmail.com>
2013-12-21 02:06:43 -0300

    Created directory structure

---
+++ plg_cck_field_gokei_player/tmpl/edit.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* @version 		Seblod 3.x
+* @package		Gokei Player Field v0.1
+* @author       http://fb.com/agjunior
+* @copyright	Copyright (C) 2013 José Agnaldo Jr.. All Rights Reserved.
+* @license 		GNU General Public License version 2 or later; see _LICENSE.php
+**/
+
+// No Direct Access
+defined( '_JEXEC' ) or die;
+?>
